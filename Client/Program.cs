b2e1d7593152a71e6b4d35473ba963db9c6cde86
Jonatan Lifshits <djonli2003@gmail.com>
2023-09-12 15:12:32 +0300

    fix: Client connection

---
+++ Client/Program.cs
@@ -0,0 +1,67 @@
+﻿
+//TODO: Исправить ошибку при повторном отправлении сообщения 
+
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+const string serverIp = "127.0.0.1";   
+const int serverPort = 8080;
+bool isWork = true;
+
+Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+IPEndPoint serverEP = new IPEndPoint(IPAddress.Parse(serverIp), serverPort);
+
+try
+{
+    socket.Connect(serverEP);
+
+    while (isWork)
+    {
+        string? message = string.Empty;
+        do
+        {
+            Console.Clear();
+            Console.Write("Enter your message: ");
+            message = Console.ReadLine();
+        } while (string.IsNullOrWhiteSpace(message));
+
+        if (message != null)
+        {
+            socket.Send(Encoding.UTF8.GetBytes(message));
+
+            byte[] buffer = new byte[256];
+            int byteCount = 0;
+            string response = string.Empty;
+
+            do
+            {
+                byteCount = socket.Receive(buffer);
+                response += Encoding.UTF8.GetString(buffer, 0, byteCount);
+
+            } while (socket.Available > 0);
+
+            Console.WriteLine($"Response from Server: {response}");
+
+            Console.WriteLine("\n\nIf u wanna exit the app press 'ESC' \n\n");
+
+            ConsoleKeyInfo keyInfo = Console.ReadKey();
+
+            if (keyInfo.Key == ConsoleKey.Escape)
+                isWork = false; 
+
+            if (!isWork)
+            {
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+        } 
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"ERROR: {ex.Message}");
+
+}
+
+Console.ReadLine();
---
+++ ClientServerApp/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Timers;
+using System.Threading.Tasks;
+using Timer = System.Timers.Timer;
+
+const string serverIp = "127.0.0.1";
+const int port = 8080;
+
+Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(serverIp), port);
+
+try
+{
+    socket.Bind(endPoint);
+    socket.Listen();
+
+    Console.WriteLine($"Server started {serverIp}:{port}");
+
+    await RunProcessAsync();
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error: {ex.Message}");
+}
+
+async Task RunProcessAsync()
+{
+    while (true)
+    {
+        Socket remoteSocket = await socket.AcceptAsync();
+
+        if (remoteSocket.RemoteEndPoint is IPEndPoint remoteEP)
+        {
+            Console.WriteLine($"Connection opened for remote: {remoteEP.Address}:{remoteEP.Port}");
+        }
+
+        // Запускаем обработку запроса с таймером
+        _ = Task.Run(() => HandleRequest(remoteSocket));
+    }
+}
+
+void HandleRequest(Socket remoteSocket)
+{
+    byte[] buffer = new byte[256];
+    int count = 0;
+    string message = string.Empty;
+
+    Timer timer = new Timer(120000); // Таймер на 2 минуты
+    timer.AutoReset = false; // Не повторять после истечения времени
+    timer.Elapsed += (sender, e) =>
+    {
+        // Закрываем соединение, если не получено сообщение в течение 2 минут
+        remoteSocket.Shutdown(SocketShutdown.Both);
+        remoteSocket.Close();
+        Console.WriteLine("Connection closed due to inactivity");
+    };
+    timer.Start();
+
+    do
+    {
+        count = remoteSocket.Receive(buffer);
+        message += Encoding.UTF8.GetString(buffer, 0, count);
+
+        // Сбросить таймер каждый раз, когда получаем данные
+        timer.Stop();
+        timer.Start();
+    } while (remoteSocket.Available > 0);
+
+    Console.WriteLine($"{DateTime.Now.ToShortTimeString()}: {message}");
+
+    string response = "Everything is ok!";
+    remoteSocket.Send(Encoding.UTF8.GetBytes(response));
+}
